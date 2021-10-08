eea9fad6bb4046a4933a85ef11a0837d9347a0fb
Muhammed Lateef <92058597+Lateef95@users.noreply.github.com>
2021-10-08 23:35:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,164 @@
+'''
+Text type (String)
+'''
+s = 'This is a single line string'
+#print (s)
+#print (type(s))
+
+#============================
+
+s = '''This is a multi line
+string example '''
+#print (s)
+
+# =================================
+
+#find character by index 
+s = 'string sample'
+#print (s[5])
+
+#===============================
+
+#slicing 
+s = 'string sample'
+#print (s[7:9])
+
+# ================================
+
+'''
+Numeric type (interger, float, complex)
+'''
+
+
+
+#=====================================
+
+'''
+sequence type (List, tuple, range)
+'''
+
+#list - homogeneous
+x = [100, 2.5, 'hello']
+#print (x)
+#print (x[1])
+
+#mutable 
+x [2] = 'python'
+#print (x)
+
+#======================================
+
+#tuple - Heterogenous 
+
+#tuple = ()
+#tuple = ('cat',[4,3,2],(1,2,3))
+
+tuple = 'hello'
+#print = (type(tuple))
+
+# a comma makes a string into a tuple 
+
+#======================================
+
+tuple = ('cat',[4,3,2],(1,2,3))
+#tuple within a tuple 
+#tuples are immutable 
+
+#========================================
+#range 
+x = range(10)
+for n in x: 
+  #print (n)
+
+#==========================================
+
+'''
+mapping type (dictionary)
+'''
+
+#dict = {}
+
+dict = {'name': 'Muahmmed' , 'age';26}
+print(dict['age'])
+dictionaries are mutable 
+
+# =================================
+
+'''
+set types 
+'''
+
+set = {}
+set = {1,2,3}
+
+#empty set 
+set = set {} 
+
+#no duplicates 
+#mutable 
+
+#====================================
+
+'''
+Booleans (True or false) 
+'''
+#print(type(True))
+
+#boolean as numbers 
+#print (True==1)
+#print (False==0)
+
+#not boolean operator 
+#print (not False)
+#print (not True)
+
+#and boolean opertor 
+#print (True and False) false 
+#print (True and True) True
+#print (False and False) False 
+
+# or boolean opertor 
+#print (True or False) false 
+#print (True or True) True
+#print (False or False) False 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
